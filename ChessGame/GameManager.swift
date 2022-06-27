6f4c5d3cbdfd84bcba866da0ed97694ed2af24c8
celine.jin(장유진)/kakao <celine.jin@kakaocorp.com>
2022-06-27 22:32:49 +0900

    [Step2] GameManager 추가

--- ChessGame/GameManager.swift
+++ ChessGame/GameManager.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class GameManager {
+    var board: Board
+    var player1: Player
+    var player2: Player
     
+    init() {
+        board = Board()
+        player1 = Player(team: .black, score: 0)
+        player2 = Player(team: .white, score: 0)
+    }
+    
+    func reset() {
+        board.reset()
+    }
+    
+    func input() {
+        // A1 -> convert -> board.move
+    }
 }
---
+++ ChessGame/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  ChessGame
+//
+//  Created by celine on 2022/06/27.
+//
+
+import Foundation
+
+struct Player {
+    var team: Team
+    var score: Int
+}
